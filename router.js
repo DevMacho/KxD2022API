25bc7b9fafba0bfbcd0893c88f0497da2ec6da14
nyaco0125 <catpacross@gmail.com>
2022-07-30 00:18:55 +0900

    push

--- router.js
+++ router.js
@@ -28,7 +28,7 @@ router.get('/addHeart/:botId/:userId', (req, res, next) => {
             if (bot.heartUsers.includes(userId)) {
                 res.status(200).json({message : "이미 하트를 누른 유저입니다"})
             } else {
-                bot.heartUsers = [...bot.heartUsers, userId]
+                bot.heartUsers.push(userId)
                 res.status(200).json({message : "하트 추가가 완료됐습니다"})
             }
     })
